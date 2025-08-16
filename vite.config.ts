1098e20a0118b0203e2b9ad91fe3f8751a01f79f
Mutlu Cansever <mucansever@gmail.com>
2025-08-17 00:42:13 +0300

    feat: fix react site

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/mucansever.github.io/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 })
