8c33eb09f812bcae346917a943fd485f90103808
Mutlu Cansever <mucansever@gmail.com>
2025-12-24 19:00:05 +0300

    Fix layout issues

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -11,20 +11,30 @@ const About: React.FC = () => {
           </p>
           
           <p>
-          I'm a product engineer with technical depth and over 2.5 years of experience in the FinTech and Mobile Gaming domains.
+          I'm a software engineer with technical depth and over 2.5 years of experience in the FinTech and Mobile Gaming domains.
           </p>
 
           <p>
-          My main areas of interest are distributed systems and computer networks, and I particularly enjoy learning how things work at a low level.
-          </p>
-
-          <p>
-          I'm currently based in Paris. Previously, I lived in Istanbul, Ankara and Milan.
+          My main areas of interest are distributed systems and computer networks. I particularly enjoy learning how things work at a low level.
           </p>
 
           <p>
           Feel free to connect with me on <a href="https://linkedin.com/in/mucansever" target="_blank" rel="noopener noreferrer">LinkedIn</a>, <a href="https://github.com/mucansever" target="_blank" rel="noopener noreferrer">GitHub</a>, or via <a href="mailto:mucansever@gmail.com">Email</a>.
           </p>
+
+          <p>
+            Here are some of my favorite aricles that shaped my perspective:
+          </p>
+
+          <div className="favorite-articles">
+            <ul>
+              <li><a href="https://martinfowler.com/articles/ship-show-ask.html" target="_blank" rel="noopener noreferrer">Ship show ask</a></li>
+              <li><a href="https://entropicthoughts.com/transparent-leadership-beats-servant-leadership" target="_blank" rel="noopener noreferrer">Transparent leadership beats servant leadership</a></li>
+              <li><a href="https://loggingsucks.com" target="_blank" rel="noopener noreferrer">Logging sucks</a></li>
+              <li>1 Topic 1 Producer N Consumer</li>
+              <li><a href="https://sre.google/sre-book/table-of-contents/" target="_blank" rel="noopener noreferrer">SRE book</a></li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
