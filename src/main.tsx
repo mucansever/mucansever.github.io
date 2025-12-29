f10a3757fd887195357dae0f4851dd970290f36a
Mutlu Cansever <36568923+mucansever@users.noreply.github.com>
2025-12-29 17:36:20 +0100

    Chore: UI improvements (#2)
    * chore: ui improvements
    
    * chore: build

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 import './index.css'
 
@@ -17,8 +17,8 @@ document.head.appendChild(googleFontsLink);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>,
 )
