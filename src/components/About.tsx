86403d17dbc6e7f9fbd8cb9de99f9d1b069099c6
Mutlu Cansever <mucansever@gmail.com>
2025-12-30 13:16:31 +0300

    chore: refactor page content

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -11,7 +11,11 @@ const About: React.FC = () => {
           </p>
           
           <p>
-          I'm a software engineer with technical depth and over 2.5 years of experience in the FinTech and Mobile Gaming domains. Currently based in Paris, France.
+          I'm a software engineer with technical depth and over 2.5 years of experience in the FinTech and Mobile Gaming domains. 
+          </p>
+          
+          <p>
+          Currently based in Paris, pursuing a Master's degree in Computer Science at <a href="https://www.ip-paris.fr/en">Institut Polytechnique de Paris</a>.
           </p>
 
           <p>
@@ -19,7 +23,7 @@ const About: React.FC = () => {
           </p>
 
           <p>
-          Feel free to connect with me on <a href="https://linkedin.com/in/mucansever" target="_blank" rel="noopener noreferrer">LinkedIn</a>, <a href="https://github.com/mucansever" target="_blank" rel="noopener noreferrer">GitHub</a>, or via <a href="mailto:mucansever@gmail.com">Email</a>.
+          Feel free to connect with me through <a href="https://linkedin.com/in/mucansever" target="_blank" rel="noopener noreferrer">LinkedIn</a>, <a href="https://github.com/mucansever" target="_blank" rel="noopener noreferrer">GitHub</a>, or <a href="mailto:mucansever@gmail.com">Email</a>.
           </p>
         </div>
       </div>
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -5,9 +5,20 @@ const Projects: React.FC = () => {
       
       <div className="project-item">
         <div className="item-title">
+          <a href="https://github.com/mucansever/widelogger" target="_blank" rel="noopener noreferrer">widelogger</a>
+        </div>
+        <div className="item-subtitle">Go Package</div>
+        <div className="item-date">Dec 2025</div>
+        <div className="item-description">
+          Trying a new standard to logging in Go applications.
+        </div>
+      </div>
+
+      <div className="project-item">
+        <div className="item-title">
           <a href="https://github.com/orgs/GaitAnalysis/repositories" target="_blank" rel="noopener noreferrer">Gait analysis with single camera setup</a>
         </div>
-        <div className="item-subtitle">Graduation Project</div>
+        <div className="item-subtitle">Undergrad Graduation Project</div>
         <div className="item-date">Sep 2022 - May 2023</div>
         <div className="item-description">
           Developed a gait analysis system using a single camera setup to extract gait parameters for medical applications. Utilized computer vision techniques and machine learning algorithms to analyze walking patterns and identify abnormalities.          
@@ -16,9 +27,20 @@ const Projects: React.FC = () => {
 
       <div className="project-item">
         <div className="item-title">
+          <a href="https://github.com/mucansever/gittree" target="_blank" rel="noopener noreferrer">gittree</a>
+        </div>
+        <div className="item-subtitle">CLI Tool</div>
+        <div className="item-date">Feb 2023</div>
+        <div className="item-description">
+          Visualize git branches in tree view.
+        </div>
+      </div>
+
+      <div className="project-item">
+        <div className="item-title">
           <a href="https://github.com/mucansever/kmeans-recommender" target="_blank" rel="noopener noreferrer">How to build a recommendation system?</a>
         </div>
-        <div className="item-subtitle">Experimental Project</div>
+        <div className="item-subtitle">Tutorial</div>
         <div className="item-date">Jan 2023</div>
         <div className="item-description">
           Guide on building Simple K-Means recommendation models.
@@ -40,7 +62,7 @@ const Projects: React.FC = () => {
         <div className="item-title">
           <a href="https://github.com/mucansever/huffman_coding" target="_blank" rel="noopener noreferrer">Huffman Encoder/Decoder</a>
         </div>
-        <div className="item-subtitle">Experimental Project</div>
+        <div className="item-subtitle">Undergrad Project</div>
         <div className="item-date">Feb 2021</div>
         <div className="item-description">
           Basic compression algorithm written in C++.
