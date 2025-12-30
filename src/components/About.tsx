e3b47edaaebbf58d68cd3da8a8a134bc8c03d18c
Mutlu Cansever <mucansever@gmail.com>
2025-12-30 12:55:36 +0300

    chore: remove articles

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -21,20 +21,6 @@ const About: React.FC = () => {
           <p>
           Feel free to connect with me on <a href="https://linkedin.com/in/mucansever" target="_blank" rel="noopener noreferrer">LinkedIn</a>, <a href="https://github.com/mucansever" target="_blank" rel="noopener noreferrer">GitHub</a>, or via <a href="mailto:mucansever@gmail.com">Email</a>.
           </p>
-
-          <p>
-            Here are some of my favorite aricles that shaped my perspective:
-          </p>
-
-          <div className="favorite-articles">
-            <ul>
-              <li><a href="https://martinfowler.com/articles/ship-show-ask.html" target="_blank" rel="noopener noreferrer">Ship show ask</a></li>
-              <li><a href="https://entropicthoughts.com/transparent-leadership-beats-servant-leadership" target="_blank" rel="noopener noreferrer">Transparent leadership beats servant leadership</a></li>
-              <li><a href="https://loggingsucks.com" target="_blank" rel="noopener noreferrer">Logging sucks</a></li>
-              <li>1 Topic 1 Producer N Consumer</li>
-              <li><a href="https://sre.google/sre-book/table-of-contents/" target="_blank" rel="noopener noreferrer">SRE book</a></li>
-            </ul>
-          </div>
         </div>
       </div>
     </div>
